27d4c69b56b2c2432a8f4942d4ddac31fcdd9ab2
ali <a.forghani.y79@gmail.com>
2021-01-18 22:47:26 +0330

    Initial commit

---
+++ src/test/java/com/moon/trackingsystem/TrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.trackingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

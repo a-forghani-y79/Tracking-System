ff3ecb27d87bca5fbd7ec9c99d6297fa374062d6
Lulz <lulz.most.of.the.time@gmail.com>
2021-01-31 20:23:45 +0330

    clean up

--- src/main/java/com/moon/trackingsystem/models/login/LoginRepository.java
+++
@@ -1,6 +0,0 @@
-package com.moon.trackingsystem.models.login;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepository extends JpaRepository<Login,Integer> {
-}

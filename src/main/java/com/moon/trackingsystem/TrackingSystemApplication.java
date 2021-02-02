4544afafca2d4a9d8bd3c3af44bcbc7c1d7dfc7d
Hossein <shakryhsyn1@gmail.com>
2021-02-02 15:11:03 +0330

    add generate the random string method

--- src/main/java/com/moon/trackingsystem/TrackingSystemApplication.java
+++ src/main/java/com/moon/trackingsystem/TrackingSystemApplication.java
@@ -1,6 +1,7 @@
 package com.moon.trackingsystem;
 
 import com.moon.trackingsystem.controller.UploadFile;
+import com.moon.trackingsystem.service.AdminService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,7 +12,6 @@ import java.io.File;
 public class TrackingSystemApplication {
 
     public static void main(String[] args) {
-
         new File(UploadFile.uploadDirectory).mkdir();
         SpringApplication.run(TrackingSystemApplication.class, args);
     }
--- src/main/java/com/moon/trackingsystem/service/AdminService.java
+++ src/main/java/com/moon/trackingsystem/service/AdminService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class AdminService {
@@ -95,16 +96,32 @@ public class AdminService {
         return projectRepository.findAll();
     }
 
-    public String invitePerson(String email) throws IOException, MessagingException {
+    public void invitePerson(String email) throws IOException, MessagingException {
+        String tokenCode = generateTheTokenCode();
         SendEmailModel sendEmailModel = new SendEmailModel();
-        sendEmailModel.setDescription("hello");
-        sendEmailModel.setSubject("aaa");
+        sendEmailModel.setDescription("http://localhost:8080/"+tokenCode);
+        sendEmailModel.setSubject("لینک ثبت نام");
         sendEmailModel.setTo(email);
         sendEmail.sendEmail(sendEmailModel);
-        //TODO creat token code
-        Token token = new Token().builder().code("12").email("email").build();
+        Token token = new Token().builder().code(tokenCode).email("email").build();
         tokenRepository.save(token);
-        return null;
+
     }
+    public  String generateTheTokenCode(){
+
+            String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder salt = new StringBuilder();
+            Random rnd = new Random();
+            while (salt.length() <6) { // length of the random string.
+                int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+                salt.append(SALTCHARS.charAt(index));
+            }
+            String saltStr = salt.toString();
+            return saltStr;
+
+
+    }
+
+
 
 }
